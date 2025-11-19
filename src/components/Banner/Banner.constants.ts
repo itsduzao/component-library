187761f3f601386dc93901835aa533dc6dee0c41
Eduardo duZÃO Henrique <itsduzao@outlook.com>
2025-11-19 13:44:43 +0000

    feat: add BANNER_STATUSES_SUMMARY constant to Banner component

--- src/components/Banner/Banner.constants.ts
+++ src/components/Banner/Banner.constants.ts
@@ -1,1 +1,5 @@
+import { getTypeSummary } from "../../utils/getTypeSummary";
+
 export const BANNER_STATUSES = ['success', 'info', 'warning', 'error'] as const;
+
+export const BANNER_STATUSES_SUMMARY = getTypeSummary(BANNER_STATUSES);
